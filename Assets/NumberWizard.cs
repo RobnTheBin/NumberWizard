f4b6cca66e093c93b5fbfbcfdd6ea64df7fb0afa
RobnTheBin <ubuchy@gmail.com>
2021-01-23 16:29:01 +0100

    Deleted Print String + Added Debug.Log "Pick a number"

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -8,6 +8,9 @@ public class NumberWizard : MonoBehaviour
     void Start()
     {
         Debug.Log("Welcome to number wizard, eyy");
+        Debug.Log("Pick a number");
+        Debug.Log("The Lowest number you can pick is 1");
+        Debug.Log("Highest number you can pick is 1000");
     }
 
     // Update is called once per frame
