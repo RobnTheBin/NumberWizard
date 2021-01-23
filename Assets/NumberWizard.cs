754b98f3c29f7b358dd03a43f83c567160bc0dce
RobnTheBin <ubuchy@gmail.com>
2021-01-23 17:02:04 +0100

    Up Arrow If Statement Added

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,15 +7,23 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int maxN = 1000;
+        int minN = 42;
+        
         Debug.Log("Welcome to number wizard, eyy");
         Debug.Log("Pick a number");
-        Debug.Log("The Lowest number you can pick is 1");
-        Debug.Log("Highest number you can pick is 1000");
+        Debug.Log("The Lowest number you can pick is: " + minN);
+        Debug.Log("Highest number you can pick is: " + maxN) ;
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
     }
 }
