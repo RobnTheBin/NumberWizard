03b904887e4a19a1f214fbdc35343bff2ae61708
RobnTheBin <ubuchy@gmail.com>
2021-01-23 18:12:42 +0100

    Encapsulating code + Creating a game loop

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int maxN = 1000;
-    int minN = 42;
-    int guess = 500;
+    int maxN;
+    int minN;
+    int guess;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartGame();     
+    }
+
+    void StartGame()
+    {
+        maxN = 1000;
+        minN = 42;
+        guess = 500;
+
         Debug.Log("Sup ya'll it's me, ya boy NumbaWizard");
         Debug.Log("Pick a number");
         Debug.Log("The Lowest number you can pick is: " + minN);
-        Debug.Log("Highest number you can pick is: " + maxN) ;
+        Debug.Log("Highest number you can pick is: " + maxN);
         Debug.Log("Tell me if your number is higher or lower " + guess);
         Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
-        maxN = maxN + 1; 
+        maxN = maxN + 1;
     }
 
     // Update is called once per frame
@@ -27,21 +35,22 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             minN = guess;
-            guess = (maxN + minN) / 2;
-            Debug.Log("Up = My number is higher than:" + guess);
-            Debug.Log("Down = My number is lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             maxN = guess;
-            guess = (maxN + minN) / 2;
-            Debug.Log("Up = My number is higher than:" + guess);
-            Debug.Log("Down = My number is lower than: " + guess);
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Am so smart");
-        }
-                
+            StartGame();        }
+    }
+    void NextGuess()
+    {
+        guess = (maxN + minN) / 2;
+        Debug.Log("Up = My number is higher than:" + guess);
+        Debug.Log("Down = My number is lower than: " + guess);
     }
 }
